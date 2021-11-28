13d9bfe09b1e3c971a4034dcaa5e1dc9232747eb
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2021-11-28 09:46:02 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
+// import logo from "";
 import "./App.css";
 
 function App() {
   const [value, setValue] = useState(1);
 
-  function increment() {
-    console.log("increment");
+  function increment(source: string) {
+    console.log("increment", source);
     setValue(value + 1);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://rawcdn.githack.com/IgorKvasn/nn-apex-poc-2/9006858cc5ac4f9ab2275e017940a1379dacafbe/build/static/media/logo.6ce24c58.svg"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
 
-        <button onClick={() => increment()} type="button">
+        <button onClick={() => increment("button")} type="button">
           Click me
         </button>
+
+        <div
+          onClick={() => increment("div")}
+          style={{
+            width: 100,
+            aspectRatio: "1/1",
+            backgroundColor: "orange",
+            cursor: "pointer",
+          }}
+        >
+          Click me (DIV)
+        </div>
         {value}
         <a
           className="App-link"
